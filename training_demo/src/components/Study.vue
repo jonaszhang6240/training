2f6288a40448e4019e029b0d00b4acf5fa364679
jonaszhang <jonaszhang91@gmail.com>
2021-09-08 22:04:39 +0800

    9.8-2

--- training_demo/src/components/Study.vue
+++ training_demo/src/components/Study.vue
@@ -5,14 +5,25 @@
 -->
 <template>
 	<div>
-		<Join></Join>
-		
+		<Join v-if="isJoin"></Join>
+		<div class="studyList">
+			<div class="listTitle">
+				<van-sidebar v-model="active">
+					<van-sidebar-item title="正在学习" />
+					<van-sidebar-item title="已完成学习" />
+					<van-sidebar-item title="推荐课程" />
+				</van-sidebar>
+			</div>
+            <div class="listItem">
+                <div class="classList" v-for="(i ,inx) in 9" :key="inx"></div>
+            </div>
+		</div>
 	</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { defineComponent } from 'vue'
+import { defineComponent,ref } from 'vue'
 import Join from '@/components/Join.vue'
 export default defineComponent({
 	name: 'Study',
@@ -20,12 +31,46 @@ export default defineComponent({
 		Join,
 	},
 	setup() {
-        
-
+        const isJoin = ref(false)
+        const active = ref(0)
+        return {
+            isJoin,
+            active
+        }
     },
 })
 </script>
 
 <style lang="scss" scoped>
 
+.studyList{
+overflow: scroll;
+    display: flex;
+    justify-content: space-between;
+    .listTitle{
+        width: 30%;
+
+        .van-sidebar{
+            width: 100%;
+            text-align: center;
+            height: 90vh;
+
+        }
+    }
+        .listItem{
+            overflow: scroll;
+            width: 65%;
+            height: 90vh;
+            .classList{
+                width: 95%;
+                height: 1rem;
+
+                margin-bottom: .2rem;
+                margin-top: .2rem;
+                border-radius: 6px;
+                box-shadow: 1px 2px 2px rgb(119, 116, 116);
+            }
+        }
+
+}
 </style>
--- training_demo/src/main.js
+++ training_demo/src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import '@/assets/reset.css'
 import '@/assets/rem.js'
-import { Swipe, SwipeItem ,Grid, GridItem,NavBar ,Tabbar, TabbarItem ,DropdownMenu, DropdownItem,Field, CellGroup,Button,Form  } from 'vant';
+import { Swipe, SwipeItem ,Grid, GridItem,NavBar ,Tabbar, TabbarItem ,DropdownMenu, DropdownItem,Field, CellGroup,Button,Form,Sidebar, SidebarItem  } from 'vant';
 
 
-createApp(App).use(Swipe).use(Grid).use(GridItem).use(NavBar).use(SwipeItem).use(Tabbar).use(TabbarItem).use(DropdownMenu).use(DropdownItem).use(Field).use(CellGroup).use(Button).use(Form).use(store).use(router).mount('#app')
+createApp(App).use(Swipe).use(Grid).use(GridItem).use(NavBar).use(SwipeItem).use(Tabbar).use(TabbarItem).use(DropdownMenu).use(DropdownItem).use(Field).use(CellGroup).use(Button).use(Form).use(Sidebar).use(SidebarItem).use(store).use(router).mount('#app')
